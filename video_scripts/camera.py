4042926f4eafb57ecc0a27206f22bc6ab91b1f32
rosavitiello <rosanna.m.vitiello@gmail.com>
2023-05-24 11:34:50 -0400

    video scripts for video integration

---
+++ video_scripts/camera.py
@@ -0,0 +1,54 @@
+import pyrealsense2 as rs
+import numpy as np
+
+class RealSenseCamera:
+    def __init__(self, res=(1280,720)):
+        # Configure depth and color streams
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+        self.res = res
+
+        # Get device product line for setting a supporting resolution
+        pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
+        pipeline_profile = self.config.resolve(pipeline_wrapper)
+
+        device = pipeline_profile.get_device()
+        found_rgb = False
+        for s in device.sensors:
+            if s.get_info(rs.camera_info.name) == 'RGB Camera':
+                    found_rgb = True
+                    break
+        if not found_rgb:
+            raise Exception("This camera configuration requires Depth camera with Color sensor")
+
+        # Enable Depth and Color streams from RealSense Camera
+        # self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        # self.config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+        self.config.enable_stream(rs.stream.color, self.res[0], self.res[1], rs.format.bgr8, 30)
+        self.config.enable_stream(rs.stream.depth, self.res[0], self.res[1], rs.format.z16, 30)
+
+        # Start streaming
+        self.pipeline.start(self.config)
+        align_to = rs.stream.color
+        self.align = rs.align(align_to)
+    
+    def get_frame_stream(self):
+        frames = self.pipeline.wait_for_frames()
+        aligned_frames = self.align.process(frames)
+        depth_frame = aligned_frames.get_depth_frame()
+        color_frame = aligned_frames.get_color_frame()
+        
+        if not depth_frame or not color_frame:
+            # If there is no frame, probably camera not connected, return False
+            raise Exception("Could not retrieve depth and/or color frame. Check that camera is connected.")
+
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        return depth_image, color_image
+    
+    def stop_stream(self):
+        # Stop streaming
+        self.pipeline.stop()
---
+++ video_scripts/visualize_camera_data.py
@@ -0,0 +1,68 @@
+import cv2 as cv
+import numpy as np
+from jetson_inference import poseNet, detectNet
+from jetson_utils import cudaFromNumpy
+
+from camera import RealSenseCamera
+
+pose_net = poseNet('resnet18-body', threshold=0.15)
+detect_net = detectNet(network='ssd-mobilenet-v2', threshold=0.5)
+
+camera = RealSenseCamera()
+
+colors = np.linspace(0, 255, pose_net.GetNumKeypoints()).astype(np.uint8)
+colors = np.squeeze(cv.applyColorMap(colors, cv.COLORMAP_VIRIDIS))
+alpha=0.4
+
+try:
+    while True:
+        _, img = camera.get_frame_stream()
+        height = img.shape[0]
+        width = img.shape[1]
+
+        frame = cudaFromNumpy(img)
+
+        poses = pose_net.Process(frame)
+        detections = detect_net.Detect(frame, width, height)
+
+        overlay = img.copy()
+        for detect in detections:
+            ID=detect.ClassID
+            y=detect.Top
+            x=detect.Left
+            y2=detect.Bottom
+            x2=detect.Right
+            item=detect_net.GetClassDesc(ID)
+            conf=detect.Confidence 
+
+            cv.putText(img, f"{item}: {conf}", (int(x) + 5, int(y) + 25), 0, 0.8, (255, 255, 255), 2)
+            cv.rectangle(overlay, (int(x), int(y)), (int(x2), int(y2)), ID,-1)
+        
+        img = cv.addWeighted(overlay, alpha, img, 1-alpha, 0)
+
+        for pose in poses:
+            keypoints = pose.Keypoints
+            links = pose.Links
+
+            for link in links:
+                kp1 = keypoints[link[0]]
+                kp2 = keypoints[link[1]]
+                kp1_color = (int(colors[kp1.ID][0]), int(colors[kp1.ID][1]), int(colors[kp1.ID][2]))
+                kp2_color = (int(colors[kp2.ID][0]), int(colors[kp2.ID][1]), int(colors[kp2.ID][2]))
+
+                # draw link
+                cv.line(img, (int(kp1.x), int(kp1.y)), (int(kp2.x), int(kp2.y)), kp1_color, 3)
+
+                # draw keypoints
+                cv.ellipse(img, (int(kp1.x), int(kp1.y)), (10,10), 0, 0, 360, kp1_color, -1)
+                cv.ellipse(img, (int(kp2.x), int(kp2.y)), (10,10), 0, 0, 360, kp2_color, -1)
+
+        cv.namedWindow('pose estimation', cv.WINDOW_AUTOSIZE)
+        cv.imshow('pose estimation', img)
+        
+        key = cv.waitKey(1)
+        if key == 27:
+            break
+finally:
+    print("Finishing up!")
+    camera.stop_stream()
